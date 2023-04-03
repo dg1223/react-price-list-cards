87f32e0955c22aba09edf81e9065f3cac4a5f9c2
dg1223 <alavi1223@hotmail.com>
2023-04-03 19:06:43 -0400

    added fake json data

---
+++ src/components/Pricelist/Pricelist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pricelist = () => {
+  return (
+    <div>
+      <h2 className="text-6xl bg-purple-300 font-bold p-4 text-center text-purple-950">
+        Awesome Affordable Prices
+      </h2>
+    </div>
+  );
+};
+
+export default Pricelist;
